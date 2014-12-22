d9504f5c552410cb46ee43968adf1086fc88ad02
Lu, Yu <luyu@fluyu.com>
2014-12-22 12:48:08 +0800

    create-dt.js working for a minimal test.

--- scripts/qcom_android/create-dt.js
+++ scripts/qcom_android/create-dt.js
@@ -46,7 +46,7 @@ var OF_DT_END_NODE = 0x2;
 var OF_DT_PROP = 0x3;
 var OF_DT_END = 0x9
 
-function examine_dtb(file_path, cb) {
+function examine_dtb(file_path, cb, fincb) {
 	fs.open(file_path, 'r', function(err, fd) {
 	    if (err) {
 	        console.log("Failed to read file: " + err.message);
@@ -74,12 +74,12 @@ function examine_dtb(file_path, cb) {
 			var version = header.readUInt32BE(20);
 			var last_comp_version = header.readUInt32BE(24);
 
-			console.log('Version: ' + version);
-			console.log('CompVer: ' + last_comp_version);
-			console.log('off_dt_struct: ' + off_dt_struct);
-			console.log('off_dt_strings: ' + off_dt_strings);
-			console.log('off_mem_rsvmap: ' + off_mem_rsvmap);
-			console.log();
+			// console.log('Version: ' + version);
+			// console.log('CompVer: ' + last_comp_version);
+			// console.log('off_dt_struct: ' + off_dt_struct);
+			// console.log('off_dt_strings: ' + off_dt_strings);
+			// console.log('off_mem_rsvmap: ' + off_mem_rsvmap);
+			// console.log();
 
 			var off_dt_struct_prox_len = off_dt_strings - off_dt_struct;
 			var off_dt_strings_prox_len = totalsize - off_dt_strings;
@@ -116,19 +116,207 @@ function examine_dtb(file_path, cb) {
 							name_offset = dt_struct.readUInt32BE(start_idx + 8),
 							value = dt_struct.slice(start_idx + 12, start_idx + 12 + value_size),
 							name = dt_strings.slice(name_offset).toString().split(/\u0000/)[0];
-						cb(name, value)
+						if(typeof cb === 'function') cb(name, value);
 						return Math.ceil(value_size / 4) * 4 + 12;
 					}
 					parse_node(0);
+					if(typeof fincb === 'function') fincb();
 				});
 			});
 		});
 	});
 }
 
+function build_final_image(dtbs, output, dtb_version, page_size) {
+	var result_buf = new Buffer([]),
+		magic_buf = new Buffer('QCDT'),
+		ver_buf = new Buffer(4),
+		dtb_count_buf = new Buffer(4);
+
+	var dtb_count = 0,
+		dtb_entry_buffer = new Buffer(0),
+		dtb_data_buffer = new Buffer(0),
+		dtb_offset = 12,							/* Header size */
+		dtb_data_offset = 0;
+	dtbs.forEach(function(dtb) {
+		dtb_count += dtb.info.length;
+
+		dtb.offset = dtb_data_offset;
+		dtb_data_offset += dtb.data.length;
+		dtb_data_offset = Math.ceil(dtb_data_offset / page_size) * page_size;
+
+		var dtb_data = new Buffer(dtb_data_offset - dtb.offset);
+		dtb.data.copy(dtb_data, 0);
+		dtb_data_buffer = Buffer.concat([dtb_data_buffer, dtb_data]);
+
+		// platform
+		// variant
+		// subtype
+		// rev
+		// dtb offset
+		// dtb size
+		dtb.info.forEach(function(entry) {
+			// dtb.offset this need calc offset later.
+			if(dtb_version === 1) {
+				var entry_buffer = new Buffer(20);
+				entry_buffer.writeUInt32LE(entry.platform, 0);
+				entry_buffer.writeUInt32LE(entry.variant, 4);
+				entry_buffer.writeUInt32LE(entry.rev, 8);
+				entry_buffer.writeUInt32LE(dtb.offset, 12);
+				entry_buffer.writeUInt32LE(dtb.data.length, 16);
+				dtb_entry_buffer = Buffer.concat([dtb_entry_buffer, entry_buffer]);
+			} else if(dtb_version === 2) {
+				var entry_buffer = new Buffer(24);
+				entry_buffer.writeUInt32LE(entry.platform, 0);
+				entry_buffer.writeUInt32LE(entry.variant, 4);
+				entry_buffer.writeUInt32LE(entry.subtype, 8);
+				entry_buffer.writeUInt32LE(entry.rev, 12);
+				entry_buffer.writeUInt32LE(dtb.offset, 16);
+				entry_buffer.writeUInt32LE(dtb.data.length, 20);
+				dtb_entry_buffer = Buffer.concat([dtb_entry_buffer, entry_buffer]);
+			} else throw new Error('Unknown DTB ver: ' + dtb_version);
+		})
+	});
+	dtb_offset += dtb_entry_buffer.length + 4;
+	dtb_offset = Math.ceil(dtb_offset / page_size) * page_size;
+
+	var final_buffer = new Buffer(dtb_offset + dtb_data_offset);
+	
+	final_buffer.write('QCDT', 0);					/* Magic */
+	final_buffer.writeUInt32LE(dtb_version, 4);		/* Version */
+	final_buffer.writeUInt32LE(dtb_count, 8);		/* Num of DTB */
+	dtb_entry_buffer.copy(final_buffer, 12);		/* DTB entries */
+	/* DTB end */
+	/* DTB entries padding */
+	dtb_data_buffer.copy(final_buffer, dtb_offset);	/* DTB data */
+
+	for(var i = 0; i < dtb_count; i ++) {
+		if(dtb_version === 1)
+			final_buffer.writeUInt32LE(final_buffer.readUInt32LE(12 + i * 20 + 12) + dtb_offset, 12 + i * 20 + 12);
+		else if(dtb_version === 2) 
+			final_buffer.writeUInt32LE(final_buffer.readUInt32LE(12 + i * 24 + 16) + dtb_offset, 12 + i * 24 + 16);
+		else throw new Error('Unknown DTB ver: ' + dtb_version);
+	}
+
+	fs.writeFileSync(output, final_buffer);
+}
+
+function build_dt(input_path, output, search_dt_tag, search_board_tag, force_v2, page_size) {
+	var dtbs = [ ],
+		files = fs.readdirSync(input_path).filter(function(f) { return /^.*\.dtb$/.test(f); });
+	files.forEach(function(file) {
+		if(!file.match(/^.*\.dtb$/)) return;
+		file = path.resolve(input_path, file);
+		var dtb = {
+				info: [ ],
+				data: undefined
+			},
+			qcdt_msm_data = undefined,
+			qcdt_board_data = undefined;
+		examine_dtb(file, function(prop, value) {
+			if(prop === search_dt_tag) qcdt_msm_data = value;
+			else if(prop === search_board_tag) qcdt_board_data = value;
+			else if(prop === 'model') console.log('Found ' + value + ', ' + path.basename(file));
+		}, function() {
+			if(qcdt_board_data !== undefined) dtb_version = 2;
+			else if(dtb_version === undefined) dtb_version = 1;
+			if(!qcdt_board_data) {
+				for(var i = 0; i < qcdt_msm_data.length; i += 3 * 4) dtb.info.push({
+					platform: qcdt_msm_data.readUInt32BE(i + 0),
+					variant: qcdt_msm_data.readUInt32BE(i + 4),
+					subtype: 0x0,
+					rev: qcdt_msm_data.readUInt32BE(i + 8)
+				});
+			} else {
+				for(var i = 0; i < qcdt_msm_data.length; i += 2 * 4) dtb.info.push({
+					platform: qcdt_msm_data.readUInt32BE(i + 0),
+					variant: qcdt_board_data.readUInt32BE(i + 0),
+					subtype: qcdt_board_data.readUInt32BE(i + 4),
+					rev: qcdt_msm_data.readUInt32BE(i + 4)
+				});
+			}
+			dtb.data = fs.readFileSync(file);
+			dtbs.push(dtb);
+			if(dtbs.length === files.length) 
+				build_final_image(dtbs, output, force_v2 ? 2 : dtb_version, page_size);
+		});
+	});
+}
+
 if(require.main === module) {
-	examine_dtb("test.dtb");
-} else module.exports = examine_dtb;
+	function help() {
+		console.log(process.argv[1] + " [options] -o <output file> <input DTB path>");
+		console.log("  options:");
+		console.log("  --output-file/-o     output file");
+		console.log("  --page-size/-s       page size in bytes");
+		console.log("  --dt-tag/-d          alternate QCDT_DT_TAG");
+		console.log("  --force-v2/-2        use dtb v2 format");
+		console.log("  --help/-h            this help screen");
+	}
+
+	var input_path = undefined,
+		output = undefined,
+		page_size = 2048,
+		force_v2 = false,
+		search_dt_tag = 'qcom,msm-id',
+		search_board_tag = 'qcom,board-id';
+		dtb_version = undefined;
+
+	for(var i = 2; i < process.argv.length; i ++) {
+		if((process.argv[i] === '-d') || ((process.argv[i] === '--dt-tag')))
+			search_dt_tag = process.argv[++i];
+		else if((process.argv[i] === '-o') || ((process.argv[i] === '--output-file')))
+			output = process.argv[++i];
+		else if((process.argv[i] === '-s') || ((process.argv[i] === '--page-size')))
+			page_size = parseInt(process.argv[++i]);
+		else if((process.argv[i] === '-2') || ((process.argv[i] === '--force-v2')))
+			force_v2 = true;
+		else if((process.argv[i] === '-h') || ((process.argv[i] === '--help')))
+			return help();
+		else {
+			if(input_path === undefined) input_path = process.argv[i];
+			else {
+				console.log('Invalid commandline option: ' + process.argv[i]);
+				return help();
+			}
+		}
+	}
+
+	if(!input_path) {
+		console.log('Using current directory as input path.');
+		input_path = input_path || '.';
+	}
+	if(!output) {
+		output = path.resolve('dt.img');
+		if(fs.existsSync(output)) {
+			 console.log('Default output path ' + output + ' exists.');
+			 console.log('Abort.');
+			 return help();
+		} else console.log('Using ' + output + ' as output file.');
+	}
+
+	if((!input_path) || (typeof input_path !== 'string') || 
+		(input_path.trim().length === 0) || (!fs.existsSync(input_path)) ||
+		(!fs.statSync(input_path).isDirectory())) {
+		console.log('Invalid input directory: ' + input_path);
+		return help();
+	}
+
+	if((!output) || (typeof output !== 'string') || 
+		(output.trim().length === 0) || (!fs.existsSync(path.dirname(output))) ||
+		(!fs.statSync(path.dirname(output)).isDirectory())) {
+		console.log('Invalid output directory: ' + path.dirname(output));
+		return help();
+	}
+	if(fs.existsSync(output)) {
+		if(fs.statSync(output).isDirectory()) {
+			console.log('Invalid output path: ' + output);
+			return help();
+		} else console.log('Warning: Will overwrite existing output file ' + output);
+	}
+
+	build_dt(input_path, output, search_dt_tag, search_board_tag, force_v2, page_size);
+} else module.exports = build_dt;
 
 
  //                               size
